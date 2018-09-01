81d1beab9699434468398e895448e4c315729458
LuisG <luigonzarat@gmail.com>
2018-09-01 15:11:39 -0300

    TP1Ejercicio1

---
+++ TP1Ejercicio1.php
@@ -0,0 +1,39 @@
+<html>
+<body>
+<head>
+    <title>TP1 Ejercicio 1</title>
+</head>
+
+<?php
+$numerico=10;
+$string="String1";
+$string2="String2";
+
+echo "Ejercicio 1) <br>";
+?>
+<br>
+<?php
+echo "Ejercicio A: <br>";
+echo "texto";
+echo "-$numerico-";
+echo "texto<br>";
+?>
+<br>
+<?php
+
+echo "Ejercicio B: <br>";
+echo "texto";
+echo "-$string-";
+echo "texto<br>";
+?>
+
+<br>
+<?php
+
+echo "Ejercicio C: <br>";
+echo 'texto';
+echo "-$string2-";
+echo 'texto<br>';
+?>
+</body>
+</html>
