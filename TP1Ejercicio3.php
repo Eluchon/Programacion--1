fe0731d2951e56795e4e84b55f5121e216f5411b
LuisG <luigonzarat@gmail.com>
2018-09-01 19:19:15 -0300

    Ejercicio3 Completo
    Ejercicio3 Completo

---
+++ TP1Ejercicio3.php
@@ -0,0 +1,113 @@
+<html>
+<body>
+<head>
+    <title>TP1 Ejercicio 3</title>
+</head>
+
+<?php
+
+
+echo "Ejercicio 3) <br>";
+?>
+<br>
+<?php
+
+echo "Ejercicio A: <br>";
+?>
+<br>
+<?php
+$var=1;
+echo "Contenido de la variable = $var <br>";
+while($var<11){
+    $var++;
+}
+echo "Contenido de la variable incrementada = $var <br>";
+?>
+<br>
+<?php
+
+echo "Ejercicio B: <br>";
+?>
+<br>
+<?php
+$var1="a";
+$cont1=1;
+echo "Contenido de la variable = $var1 <br>";
+while($cont1<11){
+    $var1++;
+    $cont1++;
+
+}
+echo "Contenido de la variable incrementada = $var1 <br>";
+?>
+<br>
+<?php
+
+echo "Ejercicio C: <br>";
+?>
+<br>
+<?php
+$var2="r";
+$cont2=1;
+echo "Contenido de la variable = $var2 <br>";
+while($cont2<11){
+    $var2++;
+    $cont2++;
+
+}
+echo "Contenido de la variable incrementada = $var2 <br>";
+?>
+<br>
+<?php
+
+echo "Ejercicio D: <br>";
+?>
+<br>
+<?php
+$var3="a1";
+$cont3=1;
+echo "Contenido de la variable = $var3 <br>";
+while($cont3<11){
+    $var3++;
+    $cont3++;
+
+}
+echo "Contenido de la variable incrementada = $var3 <br>";
+?>
+<br>
+<?php
+
+echo "Ejercicio E: <br>";
+?>
+<br>
+<?php
+$var4="a01";
+$cont4=1;
+echo "Contenido de la variable = $var4 <br>";
+while($cont4<11){
+    $var4++;
+    $cont4++;
+
+}
+echo "Contenido de la variable incrementada = $var4 <br>";
+?>
+<br>
+<?php
+
+echo "Ejercicio F: <br>";
+?>
+<br>
+<?php
+$var5="a90";
+$cont5=1;
+echo "Contenido de la variable = $var5 <br>";
+while($cont5<11){
+    $var5++;
+    $cont5++;
+
+}
+echo "Contenido de la variable incrementada = $var5 <br>";
+?>
+
+</body>
+</html>
