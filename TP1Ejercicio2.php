9caea0d1abc56f24a7977cfdba39e1687dca29e4
LuisG <luigonzarat@gmail.com>
2018-09-01 15:46:21 -0300

    Ejercicio a completo
    Ejercicio a completo

---
+++ TP1Ejercicio2.php
@@ -0,0 +1,57 @@
+<html>
+<body>
+<head>
+    <title>TP1 Ejercicio 2</title>
+</head>
+
+<?php
+
+
+echo "Ejercicio 2) <br>";
+?>
+<br>
+<?php
+$semana[0]="Domingo";
+$semana[1]="Lunes";
+$semana[2]="Martes";
+$semana[3]="Miercoles";
+$semana[4]="Jueves";
+$semana[5]="Viernes";
+$semana[6]="Sabado";
+
+echo "Ejercicio A: <br>";
+?>
+<br>
+<?php
+
+echo "Dias de la semana:<br>";
+?>
+<br>
+<?php
+
+for($i=0;$i<7;$i++){
+    echo "$semana[$i]<br>";
+}
+?>
+<br>
+<?php
+
+echo "Ejercicio B: <br>";
+
+echo "texto<br>";
+?>
+
+<br>
+<?php
+
+echo "Ejercicio C: <br>";
+echo 'texto<br>';
+?>
+<br>
+<?php
+
+echo "Ejercicio D: <br>";
+echo "texto<br>";
+?>
+</body>
+</html>
