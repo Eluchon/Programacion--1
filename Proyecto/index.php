694917c9039934174011f30cac6484e5c5e31bbd
Luis Gonzalez <luigonzarat@gmail.com>
2018-10-27 14:23:13 -0300

    GET funcionando 10/10, falta achicar codigo

--- Proyecto/index.php
+++ Proyecto/index.php
@@ -1,11 +1,11 @@
 <?php
-
+ header('Content-Type: text/html; charset=ISO-8859-1');
 //incluir el archivo principal
 include("Slim/Slim.php");
-include("./Connection.php");
+
 //registran la instancia de slim
 \Slim\Slim::registerAutoloader();
-//aplicacion 
+//aplicacion
 $app = new \Slim\Slim();
 
 //routing
@@ -16,41 +16,53 @@ $app = new \Slim\Slim();
 $app->get(
     '/vehiculos',function() use ($app){
     include("./Connection.php");
+    $sql2 = " SET NAMES utf8 ";
+    $ejecucionSQL2 = $conexion->prepare($sql2);
+    $ejecucionSQL2 ->execute();
     $sql = "SELECT * FROM vehiculo ";
     $ejecucionSQL = $conexion->prepare($sql);
     $ejecucionSQL ->execute();
+
     $vehiculos=$ejecucionSQL->fetchAll(PDO::FETCH_ASSOC);
     	//json
-   // print_r($vehiculos);
-    echo json_encode($vehiculos);
+   //print_r($vehiculos);
+
+  echo json_encode($vehiculos);
     }
-)->setParams(array($app));
+);
 
 $app->get(
     '/vehiculos/:nombre',function($nombre) use ($app){
     //almaceno el ID
     $id = $nombre;
     include("./Connection.php");
+    $sql2 = " SET NAMES utf8 ";
+    $ejecucionSQL2 = $conexion->prepare($sql2);
+    $ejecucionSQL2 ->execute();
     $sql = "SELECT * FROM vehiculo WHERE id = :id ";
     $ejecucionSQL = $conexion->prepare($sql);
     $ejecucionSQL ->execute(array("id"=>$id));
     $vehiculos=$ejecucionSQL->fetch(PDO::FETCH_ASSOC);
     //json
-    print_r($vehiculos);
-   // json_encode($vehiculos);
+   // print_r($vehiculos);
+
+  echo json_encode($vehiculos);
 
     }
 );
 $app->get(
     '/transportes',function() use ($app){
     include("./Connection.php");
+    $sql2 = " SET NAMES utf8 ";
+    $ejecucionSQL2 = $conexion->prepare($sql2);
+    $ejecucionSQL2 ->execute();
     $sql = "SELECT * FROM transporte ";
     $ejecucionSQL = $conexion->prepare($sql);
     $ejecucionSQL ->execute();
     $transporte=$ejecucionSQL->fetchAll(PDO::FETCH_ASSOC);
     //json
-     print_r($transporte);
-    //json_encode($transporte);
+     //print_r($transporte);
+   echo json_encode($transporte);
 
 }
 );
@@ -60,26 +72,32 @@ $app->get(
     //almaceno el ID
     $id = $nombre;
     include("./Connection.php");
+    $sql2 = " SET NAMES utf8 ";
+    $ejecucionSQL2 = $conexion->prepare($sql2);
+    $ejecucionSQL2 ->execute();
     $sql = "SELECT * FROM transporte WHERE id = :id ";
     $ejecucionSQL = $conexion->prepare($sql);
     $ejecucionSQL ->execute(array("id"=>$id));
     $transporte=$ejecucionSQL->fetch(PDO::FETCH_ASSOC);
     //json
-     print_r($transporte);
-    //json_encode($transporte);
+     //print_r($transporte);
+    echo json_encode($transporte);
 
 }
 );
 $app->get(
     '/choferes',function() use ($app){
     include("./Connection.php");
+    $sql2 = " SET NAMES utf8 ";
+    $ejecucionSQL2 = $conexion->prepare($sql2);
+    $ejecucionSQL2 ->execute();
     $sql = "SELECT * FROM chofer ";
     $ejecucionSQL = $conexion->prepare($sql);
     $ejecucionSQL ->execute();
     $chofer=$ejecucionSQL->fetchAll(PDO::FETCH_ASSOC);
     //json
-        print_r($chofer);
-       //     json_encode($chofer);
+       // print_r($chofer);
+       echo json_encode($chofer);
 
 }
 );
@@ -89,15 +107,19 @@ $app->get(
     //almaceno el ID
     $id = $nombre;
     include("./Connection.php");
+    $sql2 = " SET NAMES utf8 ";
+    $ejecucionSQL2 = $conexion->prepare($sql2);
+    $ejecucionSQL2 ->execute();
     $sql = "SELECT * FROM chofer WHERE id = :id ";
     $ejecucionSQL = $conexion->prepare($sql);
     $ejecucionSQL ->execute(array("id"=>$id));
     $chofer=$ejecucionSQL->fetch(PDO::FETCH_ASSOC);
     //json
-    print_r($chofer);
-    //json_encode($chofer);
+    //print_r($chofer);
+    echo json_encode($chofer);
 }
 );
 //inicializamos la aplicacion(API)
 $app->run();
+
 
